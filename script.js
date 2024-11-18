7b59fabde451ed969f48ef34afe7df6a5784fd8a
Yusuf Shaikh <94288456+yusufbigweb@users.noreply.github.com>
2024-11-18 12:17:43 +0000

    Update -  by Yusuf

---
+++ script.js
@@ -0,0 +1,12 @@
+// Global Varible Selctor
+
+const openBtn = document.querySelector('.open');
+const btnSign = document.querySelector(".btn")
+
+
+// Event Function
+
+openBtn.addEventListener('click', function(){
+    console.log("yes it's working perfactly.")
+
+})
